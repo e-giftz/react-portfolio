19e658577d1c0abccab3a6a7b8d8d6c9d95cfe13
Gift <56089461+e-giftz@users.noreply.github.com>
2021-07-29 14:12:10 -0400

    Uploading profile photo and updating about me pages

--- my-portfolio/src/components/Profile.js
+++ my-portfolio/src/components/Profile.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import '../styles/Profile.css';
+import logo from './logo/photo-avatar.png';
 
 function Profile() {
     return (
-        <div className="profile-container">
-        <div className="profile-img" style={{ backgroundImage: "url(/photo-avatar.png)" }}>
-        </div>  
-        <h1>About Me</h1> 
-        <p>I am a creative web developer with </p>     
-        </div>
+        <>
+            <div className="profile-container">
+                <div className="profile-wrapper">
+                    <div className="logo-wrap">
+                        <img src={logo}
+                            width="150px"
+                            height="150px"
+                            style={{ display: "inline", border: "1px solid black", borderRadius: "5px ! important" }}
+                            alt="avatar"
+                        />
+                    </div>
+
+                    <h1>About Me</h1>
+                    <p>I am a creative web developer with </p>
+
+                </div>
+
+            </div>
+
+        
+        </>
     )
 }
 
